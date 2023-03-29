5b7034232df5746fa8961557562cdceb9f08e4cc
paccco <pacocarrascoconde@gmail.com>
2023-03-29 16:57:22 +0200

    Tutorial copiado

---
+++ calculadora.thrift
@@ -0,0 +1,7 @@
+service Calculadora{
+    void ping () ,
+    double suma (1: double num1 , 2: double num2 ) ,
+    double resta (1: double num1 , 2: double num2 ) ,
+    double multiplicacion(1: double num1 , 2: double num2 ) ,
+    double division(1: double num1 , 2: double num2 )
+}
---
+++ gen-py/client.py
@@ -0,0 +1,19 @@
+from calculadora import Calculadora
+from thrift import Thrift
+from thrift . transport import TSocket
+from thrift . transport import TTransport
+from thrift . protocol import TBinaryProtocol
+
+transport = TSocket . TSocket ( " localhost " , 9090)
+transport = TTransport . TBufferedTransport ( transport )
+protocol = TBinaryProtocol . TBinaryProtocol ( transport )# creamos el cliente
+client = Calculadora . Client ( protocol )
+
+transport . open ()
+print (" Hacemos ping al server ")
+client . ping ()
+resultado = client . suma (1 , 1)
+print (" 1+1= "+ str ( resultado ))
+resultado = client . resta (1 , 1)
+print (" 1-1= "+ str ( resultado ))
+transport . close ()
